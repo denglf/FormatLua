d5d6e26b230e540bb04379c0dbc2a1dadb113965
denglf <denglifen@gmail.com>
2014-05-09 20:25:32 +0800

    Support for Sublime Text 3

--- FormatLua.py
+++ FormatLua.py
@@ -6,9 +6,11 @@ import re
 from os.path import basename
 from os.path import join
 
+version = int(sublime.version())
+package = "FormatLua"
+
 def plugin_loaded():
-    package = "FormatLua"
-    if VERSION >= 3006:
+    if version >= 3006:
         package_location = os.path.join(sublime.installed_packages_path(), package + ".sublime-package")
         if not os.path.exists(package_location):
             package_location = os.path.join(os.path.dirname(sublime.executable_path()), "Packages", package + ".sublime-package")
@@ -24,7 +26,7 @@ class FormatLuaCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
         regions = view.sel()
-        # if there are more than 1 region or region one and it's not empty
+        # if there are more than 1 region or region one and it"s not empty
         if len(regions) > 1 or not regions[0].empty():
             for region in view.sel():
                 if not region.empty():
@@ -43,17 +45,17 @@ class FormatLuaCommand(sublime_plugin.TextCommand):
         # indent_char = " " #TODO indent by TAB (currently not supported in python-sqlparse)
         # indent_size = int(settings.get("tab_size")) if indent_char == " " else 1
         s = s.encode("utf-8")
-        packages_path = join(sublime.packages_path(), "FormatLua")
+        packages_path = join(sublime.packages_path(), package)
         os.chdir(packages_path)
-        tmp = 'tmp.lua'
-        fileHandle = open(tmp, 'wb')
+        tmp = "tmp.lua"
+        fileHandle = open(tmp, "wb")
         fileHandle.write(s)
         fileHandle.close()
-        settings = sublime.load_settings('FormatLua.sublime-settings')
-        lua_path = settings.get('lua_path')
-        cmd = lua_path + ' formatter.lua --file ' + tmp
+        settings = sublime.load_settings(package + ".sublime-settings")
+        lua_path = settings.get("lua_path")
+        cmd = lua_path + " formatter.lua --file " + tmp
         # print cmd
         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         sourcecode = p.stdout.read()
         os.remove(tmp)
-        return sourcecode.decode('utf-8')
+        return sourcecode.decode("utf-8")
